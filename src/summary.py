a48ce95d48adedf177ecacde1eb66863a121124b
Sharonsyra <sharonwaithii@gmail.com>
2024-07-17 19:03:01 +0300

    Add more summaries

--- src/summary.py
+++ src/summary.py
@@ -11,10 +11,22 @@ people = pd.read_sql('SELECT * FROM people', engine)
 
 size_by_place_of_birth = people.groupby('place_of_birth').size().to_dict()
 
-with open('/data/summary_by_pob.json', 'w') as f:
+with open('/data/people_size_by_pob.json', 'w') as f:
     json.dump(size_by_place_of_birth, f)
 
-summary_by_date_of_birth = people.groupby('place_of_birth').first().to_dict()
+first_by_place_of_birth = people.groupby('place_of_birth').first().to_dict()
 
-with open('/data/summary_by_dob.json', 'w') as f:
-    json.dump(summary_by_date_of_birth, f)
+with open('/data/first_by_place_of_birth.json', 'w') as f:
+    json.dump(first_by_place_of_birth, f)
+
+places = pd.read_sql('SELECT * FROM places', engine)
+
+places_size_per_country = places.groupby('country').size().to_dict()
+
+with open('/data/places_size_per_country.json', 'w') as f:
+    json.dump(places_size_per_country, f)
+
+first_per_country = places.groupby('country').first().to_dict()
+
+with open('/data/first_per_country.json', 'w') as f:
+    json.dump(first_per_country, f)
