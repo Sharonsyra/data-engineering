256bc72480810118c171d7fcfafe79c707082ded
Sharonsyra <sharonwaithii@gmail.com>
2024-07-17 19:18:11 +0300

    Places with more than 100 people

--- src/summary.py
+++ src/summary.py
@@ -30,3 +30,9 @@ first_per_country = places.groupby('country').first().to_dict()
 
 with open('/data/first_per_country.json', 'w') as f:
     json.dump(first_per_country, f)
+
+places_grouped = people.groupby('place_of_birth').size()
+places_with_more_than_100_people = places_grouped[places_grouped > 100].to_dict()
+
+with open('/data/places_with_more_than_100_people.json', 'w') as f:
+    json.dump(places_with_more_than_100_people, f)
